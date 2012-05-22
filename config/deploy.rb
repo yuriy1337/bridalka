2fc8709eb66c2de272450812f1b3783e6044a18f
Yuriy Myronchenko <yuriy1337@gmail.com>
2012-05-21 19:39:58 -0500

    added more deployement stuff

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,15 @@
 require "bundler/capistrano"
 require "rvm/capistrano"
 
+Setup the server details
+set :domain, "bridalka.dreamhosters.com"
+set :application, "Bridalka"
+
 #Setup SCM details.
 set :scm, :git
-set :application, "Bridalka"
+set :branch, 'master'
 set :repository,  "git@github.com:yuriy1337/brikalka.git"
+set :deploy_via, :remote_cache
 
 #set ruby string - not sure if necessayry
 set :rvm_ruby_string, 'ree@rails3'
@@ -18,9 +23,9 @@ set :rvm_install_type, :stable
 before 'deploy:setup', 'rvm:install_rvm'	#automatically install rvm
 before 'deploy', 'rvm:install_rvm'	#update rvm each time
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+role :web, domain   # Your HTTP server, Apache/etc
+role :app, domain   # This may be the same as your `Web` server
+role :db,  domain, :primary => true # This is where Rails migrations will run
 #role :db,  "your slave db-server here"
 
 # if you want to clean up old releases on each deploy uncomment this:
@@ -30,10 +35,10 @@ role :db,  "your primary db-server here", :primary => true # This is where Rails
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+   task :start do ; end
+   task :stop do ; end
+   task :restart, :roles => :app, :except => { :no_release => true } do
+     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+   end
+ end
