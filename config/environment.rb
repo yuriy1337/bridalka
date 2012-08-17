d4b4488a83ca7b38a49e8ea65eab4f89024580c5
Yuriy Myronchenko <yuriy1337@gmail.com>
2012-08-16 19:04:32 -0500

    added load config

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-APP_CONFIG = YAML.load_file("#{RAILS_ROOT}/config/config.yml")[RAILS_ENV]
-
 # Initialize the rails application
 Bridalka::Application.initialize!
