025bdc587f19d6ce0533be5200d75158e717651c
yuriy1337 <yuriy1337@gmail.com>
2012-11-01 23:46:37 -0500

    updated jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,6 @@
 //for some reason including all this here breaks galleriffic 
 //= require jquery
-//= require jqueryui/jquery-ui-1.8.18.custom.min.js
+//= require jqueryui/jquery-ui-1.9.1.custom.min.js
 //= require jquery_ujs
 //= require_tree .
 //= require global
