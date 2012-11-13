03bba69cea64a740fd0f185fbbcf41f9104c8471
yuriy1337 <yuriy1337@gmail.com>
2012-11-12 21:42:49 -0600

    store images per gallery

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -18,7 +18,7 @@ after :remove, :delete_empty_dir
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/galleries/#{model.gallery_id}/#{model.class.to_s.underscore}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
